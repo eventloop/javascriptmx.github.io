39caffee7eeb82853d1e0df3b652e33e0d4b5209
Jeduan Cornejo <jeduan@gmail.com>
2015-04-04 15:07:41 -0600

    removing js and logo is now svg

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,39 @@
-/*jshint quotmark:false, maxlen:false, browser:true */
-
-var headerSizer = function () {
-	$('header, .color-overlay').css('min-height', window.innerHeight);
-}
-
-$(window).on('load', headerSizer);
-$(window).on('resize', headerSizer);
+// function headerSizer () {
+//   $('header, .color-overlay').css('min-height', window.innerHeight);
+// }
+//
+// $(window).on('load', headerSizer);
+// $(window).on('resize', headerSizer);
 
 if (matchMedia('(max-width: 480px)').matches) {
-	$('.main-navigation a').on('click', function () {
-		$(".navbar-toggle").click();
-	});
+  $('.main-navigation a').on('click', function () {
+    $(".navbar-toggle").click();
+  });
 }
 
+var mainNav
+$(document).ready(function () {
+  mainNav();
+});
 
-if(window.home){
-	var mainNav
-	$(document).ready(function () {
-		mainNav();
-	});
-
-	$(window).scroll(function () {
-		mainNav();
-	});
-}
+$(window).scroll(function () {
+  mainNav();
+});
 
 if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
-	mainNav = function mainNav() {
-		var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-		if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
+  mainNav = function mainNav() {
+    var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
 
-		else $('.sticky-navigation').stop().animate({"top": '-60'});
-	}
+    else $('.sticky-navigation').stop().animate({"top": '-60'});
+  }
 }
 
 if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
-	mainNav = function mainNav() {
-		var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-		if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
+  mainNav = function mainNav() {
+    var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
 
-		else $('.sticky-navigation').stop().animate({"top": '-120'});
-	}
+    else $('.sticky-navigation').stop().animate({"top": '-120'});
+  }
 }
